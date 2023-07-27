355816323b40e512334d7fe06d3502a637eb6a47
Ronwez <alexisleop@gmail.com>
2023-07-27 14:27:27 +0200

    filtres

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
+//AFICHER LA GALLERIE
+
 async function afficherDonneesApi() {
   try {
     const Apiworks = await fetch("http://localhost:5678/api/works");
@@ -7,7 +9,7 @@ async function afficherDonneesApi() {
     const affichage_title = document.querySelector("#title");
 
     for (let i = 0; i < response.length; i++) {
-      const { id, title, imageUrl } = response[i];
+      const { title, imageUrl } = response[i];
 
       const itemHtml = `
         <div class="item">
@@ -24,22 +26,65 @@ async function afficherDonneesApi() {
 
 afficherDonneesApi();
 
+//FILTRES
 
-
-const Apicategories = async () =>{
-  const response = await fetch("http://localhost:5678/api/categories");
-  const jsonData = await response .json();
-  return jsonData;
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (err) {
+    throw new Error(`Erreur lors de la récupération des données : ${err}`);
   }
+}
 
-  function filterObjets() {
-    const galleryItems = document.querySelectorAll(".gallery .item");
-    galleryItems.forEach(item => {
-      const categoryId = parseInt(item.getAttribute("data-category-id"));
-      if (categoryId === 1) {
-        item.style.display = "block"; 
-      } else {
-        item.style.display = "none"; 
-      }
-    });
+function afficherImages(response) {
+  const affichage_image = document.querySelector(".gallery");
+  affichage_image.innerHTML = "";
+  for (const { title, imageUrl } of response) {
+    const itemHtml = `
+      <div class="item">
+        <img src="${imageUrl}">
+        <figcaption>${title}</figcaption>
+      </div>
+    `;
+    affichage_image.insertAdjacentHTML("beforeend", itemHtml);
   }
+}
+
+async function filtrerParCategorie(categorieId) {
+  try {
+    const response = await fetchData("http://localhost:5678/api/works");
+    const filteredResponse = response.filter((item) => item.categoryId === categorieId);
+    afficherImages(filteredResponse);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function afficherToutesLesDonnees() {
+  try {
+    const response = await fetchData("http://localhost:5678/api/works");
+    afficherImages(response);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function ajouterGestionnairesFiltres() {
+  try {
+    const categoriesData = await fetchData("http://localhost:5678/api/categories");
+    const btnAll = document.querySelector(".btnfilall");
+    const btnObjets = document.querySelector(".btnfil1");
+    const btnAppartements = document.querySelector(".btnfil2");
+    const btnHotelsRestaurants = document.querySelector(".btnfil3");
+
+    btnAll.addEventListener("click", afficherToutesLesDonnees);
+    btnObjets.addEventListener("click", () => filtrerParCategorie(1));
+    btnAppartements.addEventListener("click", () => filtrerParCategorie(2));
+    btnHotelsRestaurants.addEventListener("click", () => filtrerParCategorie(3));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+ajouterGestionnairesFiltres();
